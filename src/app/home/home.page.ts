29408e2e5ec77a6e369f3d0a1b906c304c7bcb95
Rodrigo Freire Machado <rodrigofreire0210@gmail.com>
2023-05-22 16:13:46 -0300

    IonicAlcoolGasolina-V1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  public resultado: string='...';
+  public precoAlcool: string='';
+  public precoGasolina: string='';
+
+  public calcular(){
+    if(this.precoAlcool && this.precoGasolina){
+      var pAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+
+      if(pAlcool/pGasolina>=0.7){
+        this.resultado = 'Gasolina é melhor!'
+      }else{
+        this.resultado = 'Alcool é melhor!'
+      }
+    }else{
+      this.resultado = 'Não Preenchido'
+    }
+  }
 }
